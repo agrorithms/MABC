0122cc4afcdcc8778b8d6d1f662075b54b627c86
agrorithms <louisagrogit+git@gmail.com>
2025-09-12 23:34:43 +0000

    bankHours update

--- exercises/bankHours/bankHours.py
+++ exercises/bankHours/bankHours.py
@@ -1,36 +1,53 @@
+from collections import namedtuple
 
-def isInRange(openHours,targetHours):
-        return targetHours[0]>=openHours[0] and targetHours[1]<=openHours[1]
+class TimeRange:
+    def __init__(self,val1:float,val2:float):
+        self.open=val1
+        self.close=val2
+    def __lt__(self,other):
+        return self.open<other.open
+    def __eq__(self,other):
+        if not isinstance(other,TimeRange):
+            return False
+        return self.close==other.close and self.open==other.open
+    
+
+def isInRange(openHours:TimeRange,targetHours:TimeRange):
+        '''
+        assume open is always less than close based on tradeHoursWrap handling in the isCovered method, 
+        which is the only place this function is called
+        '''
+        return targetHours.open>=openHours.open and targetHours.close<=openHours.close
 
 class BankHours:
-    def __init__(self,bankHoursList):
+    def __init__(self,bankHoursList:list[TimeRange]):
         self.updateBankHours(bankHoursList)
     
-    def updateBankHours(self,newBankHours):
+    def updateBankHours(self,newBankHours:list[TimeRange]):
         for i,bank in enumerate(newBankHours):
-            if bank[1]<bank[0]:
-                newBankHours.append([0,bank[1]])
-                newBankHours[i][1]=24
+            if bank.close<bank.open:
+                newBankHours.append(TimeRange(0,bank.close))
+                newBankHours[i].close=24
         newBankHours.sort()
         openHours=[newBankHours[0]]
         pointer=0
-        for bank in newBankHours[1:]:
+        for bankRange in newBankHours[1:]:
 
-            if bank[0]<=openHours[pointer][1]:
-                openHours[pointer][1]=max(bank[1],openHours[pointer][1])
+            if bankRange.open<=openHours[pointer].close:
+                openHours[pointer].close=max(bankRange.close,openHours[pointer].close)
             else:
-                openHours.append(bank)
+                openHours.append(bankRange)
                 pointer+=1
         self.hours=openHours
     
 
-    def isCovered(self,tradeHours):
+    def isCovered(self,tradeHours:TimeRange):
         tradeHoursWrap=None
-        if tradeHours[1]<tradeHours[0]:
-            tradeHoursWrap=[0,tradeHours[1]]
-            tradeHours=[tradeHours[0],24]
+        if tradeHours.close<tradeHours.open:
+            tradeHoursWrap=TimeRange(0,tradeHours.close)
+            tradeHours=TimeRange(tradeHours.open,24)
             wrapHoursPass=False
-            print(tradeHours,tradeHoursWrap)
+            #print(tradeHours,tradeHoursWrap)
         for openHours in self.hours:
             if isInRange(openHours,tradeHours):
                 if tradeHoursWrap==None or wrapHoursPass:
--- exercises/bankHours/test_bankHours.py
+++ exercises/bankHours/test_bankHours.py
@@ -1,19 +1,21 @@
 import pytest
+from bankHours import TimeRange
 from bankHours import BankHours
 from bankHours import isInRange
 
 
-test1= BankHours([[9,10],[9.5,12],[10,11],[19,24],[3,8],[3,7],[4,7],[2,3],[18,4],[20,6],[21,2]])
+
+test1= BankHours([TimeRange(9,10),TimeRange(9.5,12),TimeRange(10,11),TimeRange(19,24),TimeRange(3,8),TimeRange(3,7),TimeRange(4,7),TimeRange(2,3),TimeRange(18,4),TimeRange(20,6),TimeRange(21,2)])
 
 @pytest.mark.parametrize ('hours,expected', [
-    ([4,8], True),
-    ([4,9], False),
-    ([9,1], False),
-    ([18,8], True),
+    (TimeRange(4,8), True),
+    (TimeRange(4,9), False),
+    (TimeRange(9,1), False),
+    (TimeRange(18,8), True),
 
 ])
 def test_covered_order(hours,expected):
     assert test1.isCovered(hours) == expected
 
 def test_bank_hours():
-    assert test1.hours = [0, 8], [9, 12], [18, 24]
+    assert test1.hours ==[TimeRange(0, 8), TimeRange(9, 12), TimeRange(18, 24)]
